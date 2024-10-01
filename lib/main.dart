69fd4b763439dfb8acd781df3b1e71f1ea5600ce
Saifeddine Kasmi <skasmi@e1r2p9.1337.ma>
2024-10-01 22:54:30 +0100

    update by FRATELLO

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:device_preview_plus/device_preview_plus.dart';
 void main() => runApp(
       DevicePreview(
         enabled: true,
-        builder: (context) => MyApp(), // Wrap your app
+        builder: (context) => const MyApp(), // Wrap your app
       ),
     );
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,30 +15,132 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 0, 85, 103),
+      backgroundColor: Color.fromARGB(255, 0, 85, 103),
       appBar: AppBar(
         title: Center(
             child: Text(
           contactName,
-          style: const TextStyle(fontFamily: 'my', fontWeight: FontWeight.bold),
+          style: TextStyle(fontFamily: 'my', fontWeight: FontWeight.bold),
         )),
       ),
       body: Center(
         child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(
               height: 10,
             ),
             Container(
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.amber),
-              width: screenSize.width - 20,
-              height: 300,
-              child: Container(
-                height: 10,
-                width: screenSize.width - 250,
-                decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 219, 27, 27)),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 25,
+                      blurStyle: BlurStyle.normal,
+                      color: Colors.black,
+                      offset: Offset.zero,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/me.png')),
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white),
+              height: 230,
+              width: screenSize.width - 30,
+              // decoration: BoxDecoration(color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 8.0, left: 15, right: 15, top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xFFD00000),
+                        child: IconButton(
+                          icon: const Icon(
+                            size: 30,
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      const SizedBox(height: 3),
+                      const Text(
+                        'Delete',
+                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xFFF7941D),
+                        child: IconButton(
+                          icon: const Icon(
+                            size: 30,
+                            Icons.message,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      const SizedBox(height: 3),
+                      const Text(
+                        'Message',
+                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xFF28B33E),
+                        child: IconButton(
+                          icon: const Icon(
+                            size: 30,
+                            Icons.call,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      const SizedBox(height: 3),
+                      const Text(
+                        'Call',
+                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color(0xFFFF5FF9),
+                        child: IconButton(
+                          icon: const Icon(
+                            size: 30,
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      const SizedBox(height: 3),
+                      const Text(
+                        'Edit',
+                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
+                      )
+                    ],
+                  ),
+                ],
               ),
             )
           ],
