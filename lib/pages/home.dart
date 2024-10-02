f86fd30e10c51993fa053303c79964bb7eaf5a52
Saifeddine Kasmi <skasmi@e1r2p9.1337.ma>
2024-10-02 19:17:40 +0100

    update by FRATELLO

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,127 +23,136 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontFamily: 'my', fontWeight: FontWeight.bold),
         )),
       ),
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 25,
-                      blurStyle: BlurStyle.normal,
-                      color: Colors.black,
-                      offset: Offset.zero,
-                      spreadRadius: 2,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 25,
+                        blurStyle: BlurStyle.normal,
+                        color: Colors.black,
+                        offset: Offset.zero,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                    image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/images/me.png')),
+                    borderRadius: BorderRadius.circular(260),
+                    color: Colors.white),
+                height: 150,
+                width: 150,
+                // decoration: BoxDecoration(color: Colors.white),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 8.0, left: 15, right: 15, top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xFFD00000),
+                          child: IconButton(
+                            icon: const Icon(
+                              size: 20,
+                              Iconsax.profile_delete,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        const Text(
+                          'Delete',
+                          style:
+                              TextStyle(color: Colors.white, fontFamily: 'my'),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xFFF7941D),
+                          child: IconButton(
+                            icon: const Icon(
+                              size: 20,
+                              Iconsax.message,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        const Text(
+                          'Message',
+                          style:
+                              TextStyle(color: Colors.white, fontFamily: 'my'),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xFF28B33E),
+                          child: IconButton(
+                            icon: const Icon(
+                              size: 20,
+                              Iconsax.call,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        const Text(
+                          'Call',
+                          style:
+                              TextStyle(color: Colors.white, fontFamily: 'my'),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: const Color(0xFFFF5FF9),
+                          child: IconButton(
+                            icon: const Icon(
+                              size: 20,
+                              Iconsax.edit,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        const Text(
+                          'Edit',
+                          style:
+                              TextStyle(color: Colors.white, fontFamily: 'my'),
+                        )
+                      ],
                     ),
                   ],
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/me.png')),
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white),
-              height: 230,
-              width: screenSize.width - 30,
-              // decoration: BoxDecoration(color: Colors.white),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  bottom: 8.0, left: 15, right: 15, top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color(0xFFD00000),
-                        child: IconButton(
-                          icon: const Icon(
-                            size: 30,
-                            Icons.delete,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                      const SizedBox(height: 3),
-                      const Text(
-                        'Delete',
-                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color(0xFFF7941D),
-                        child: IconButton(
-                          icon: const Icon(
-                            size: 30,
-                            Icons.message,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                      const SizedBox(height: 3),
-                      const Text(
-                        'Message',
-                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color(0xFF28B33E),
-                        child: IconButton(
-                          icon: const Icon(
-                            size: 30,
-                            Icons.call,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                      const SizedBox(height: 3),
-                      const Text(
-                        'Call',
-                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
-                      )
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color(0xFFFF5FF9),
-                        child: IconButton(
-                          icon: const Icon(
-                            size: 30,
-                            Icons.edit,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                      const SizedBox(height: 3),
-                      const Text(
-                        'Edit',
-                        style: TextStyle(color: Colors.white, fontFamily: 'my'),
-                      )
-                    ],
-                  ),
-                ],
+                ),
+                
               ),
-            )
-          ],
+              Column(),
+            ],
+          ),
         ),
       ),
     );
