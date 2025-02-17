c2f950dc6ea897c354d546b4dc19658548caa4fe
FRATELLO <saife.addine123@gmail.com>
2025-02-17 17:57:00 +0100

    add translations but just a test

--- lib/translations/locale_keys.g.dart
+++ lib/translations/locale_keys.g.dart
@@ -11,5 +11,35 @@ abstract class  LocaleKeys {
   static const phone_number = 'phone_number';
   static const email = 'email';
   static const permission_denied = 'permission_denied';
+  static const retry = 'retry';
+  static const request_permission = 'request_permission';
+  static const please_grant_permission = 'please_grant_permission';
+  static const contact_added_successfully = 'contact_added_successfully';
+  static const error_adding_contact = 'error_adding_contact';
+  static const error_accessing_contacts = 'error_accessing_contacts';
+  static const permission_denied_contacts = 'permission_denied_contacts';
+  static const error = 'error';
+  static const could_not_open_messages_app = 'could_not_open_messages_app';
+  static const ok = 'ok';
+  static const error_opening_messages = 'error_opening_messages';
+  static const could_not_open_phone_app = 'could_not_open_phone_app';
+  static const error_making_call = 'error_making_call';
+  static const delete_contact = 'delete_contact';
+  static const delete_confirmation = 'delete_confirmation';
+  static const cancel = 'cancel';
+  static const delete = 'delete';
+  static const contact_deleted_successfully = 'contact_deleted_successfully';
+  static const contact_updated_successfully = 'contact_updated_successfully';
+  static const error_updating_contact = 'error_updating_contact';
+  static const could_not_find_contact = 'could_not_find_contact';
+  static const no_number_available = 'no_number_available';
+  static const no_email_available = 'no_email_available';
+  static const message = 'message';
+  static const call = 'call';
+  static const white = 'white';
+  static const blue = 'blue';
+  static const red = 'red';
+  static const green = 'green';
+  static const purple = 'purple';
 
 }
