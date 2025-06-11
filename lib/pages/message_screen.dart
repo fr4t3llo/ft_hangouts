878586955aa59ca5ea8f2806bb70468b0092b335
FRATELLO <saife.addine123@gmail.com>
2025-06-11 12:06:08 +0100

    update by FRATELLO

--- lib/pages/message_screen.dart
+++ lib/pages/message_screen.dart
@@ -34,6 +34,7 @@ class MessagePage extends StatefulWidget {
 
 
 
+
 class _MessagePageState extends State<MessagePage> with WidgetsBindingObserver {
   final TextEditingController _messageController = TextEditingController();
   late final Telephony? telephony;
